43f7df3288a6dd621a7b6d6bde3908157188b419
fano <aafano19@gmail.com>
2019-09-03 11:14:56 +0300

    adding change with user profil

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: "item#index"
   resources :item
   resources :cart
+  resources :user
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
