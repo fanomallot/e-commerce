8b02d65e164736ca931d155f36ed72b9b71ce81c
FenosoaSitraka22 <fenosoratsimba@gmail.com>
2019-09-04 19:14:04 +0300

    mailerlocal

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,13 @@
 class OrdersController < ApplicationController
+	
 	def create
-		o = Order.new(user:current_user)
+		@o = Order.new(user:current_user)
 		c = Cart.find(params[:cart_id])
 		 puts "*"*90
 		 puts c.items
 		 Order.new(user: current_user)
-			o.items = c.items
-			if o.save 
+			@o.items = c.items
+			if @o.save 
 				c.items = []
 				redirect_to "/"
 			else 
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,11 @@
 class Order < ApplicationRecord
+after_create :welcome_send
   belongs_to :user
   has_many :relationois
   has_many :items, through: :relationois
+  after_create :welcome_send
+
+  def welcome_send
+    UserMailer.command_email(self.user).deliver_now
+  end
 end
